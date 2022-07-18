16ab7fe7ace0a7362ab508c6a90781a64417282d
Raphavf <r.munizpacheco@hotmail.com>
2022-07-18 15:58:44 -0300

    Adicionando palavras randomicas

--- forca.py
+++ forca.py
@@ -1,3 +1,5 @@
+import random
+
 def jogar():
 
 
@@ -5,7 +7,19 @@ def jogar():
     print("***Bem vindo ao Jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "paralelepipedo"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero]
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     #for letra in palavra_secreta:
